853da782e460435384e1ab798bf55e7223ea8485
Merserch <merserch@sheridancollege.ca>
2026-02-15 17:02:48 -0500

    Added commenting and time-accurate values * added time.deltatime to all incrementations * changed the values to reflect intended number of seconds rather than arbitrary numbers * added explainations to each chunk of code

--- Assets/Scripts/PlantScript.cs
+++ Assets/Scripts/PlantScript.cs
@@ -1,28 +1,64 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlantScript : MonoBehaviour
 {
-    public SpriteRenderer sr;
-    public int currentLifespan = 0;
-    public int maxLifespan = 1500;
+    //current lifespan and maximum lifespan used for reflecting the plant's growth
+    public float currentLifespan = 0f;
+    public float maxLifespan = 30f;
+    //drinking used by plot script
     public bool drinking = true;
+
+    //get the sprite renderer, the plot it belongs to, the plot script of that plot, and the sprites needed
+    public SpriteRenderer sr;
     public GameObject myPlot;
+    public PlotScript plotScript;
+    public List<Sprite> stage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //get the sprite renderer so we can change the sprite
         sr = GetComponent<SpriteRenderer>();
-        //myPlot = GetComponent<PlotScript>();
+        //get the plot script so we can see if the soil is wet
+        plotScript = myPlot.GetComponent<PlotScript>();
+        //set the sprite to the first stage, a little sprout
+        sr.sprite = stage[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if(myPlot.moisture > 0)
-        // {
-        //     currentLifespan++;
-        // }
+        //check the moisture of the soil
+        if(plotScript.moisture > 0)
+        {
+            //if it's wet, it may drink and grow
+            currentLifespan += 1f * Time.deltaTime;
+            drinking = true;
+        }
 
+        //if moisture is low, drinking will not change but the lifespan will not increase
+
+        if(plotScript.moist == false)
+        {
+            //if the minimum moisture level is met, the plant will stop drinking
+            drinking = false;
+        }
+
+        //set the sprite once reaching a certain age
+        if(currentLifespan >= maxLifespan * 1 / 3)
+        {
+            //if even older, set the older sprite stage
+            if(currentLifespan >= maxLifespan * 2 / 3)
+            {
+                sr.sprite = stage[2];
+            }
+            //otherwise set the second stage sprite
+            else
+            {
+                sr.sprite = stage[1];
+            }
+        }
 
     }
 }
--- Assets/Scripts/PlotScript.cs
+++ Assets/Scripts/PlotScript.cs
@@ -5,50 +5,76 @@ using UnityEngine.UI;
 
 public class PlotScript : MonoBehaviour
 {
-    public int moisture;
+    //moisture for value, moist for plant reference
+    public float moisture;
     public bool moist;
+    //min and max values for clamping
+    public float maxMoisture = 15f;
+    public float minMoisture = -5f;
+    //watering system components, drenching value for slider value and slider itself in the scene canvas
     public float drenchingValue;
     public Slider drenchSlider;
+    //the plant object associated with this plot and it's script
+    public GameObject myPlant;
+    public PlantScript plantScript;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        moisture = 500;
-        moist = true;
+        //set to maximum moisture
+        moisture = maxMoisture;
+        //get the plant object
+        plantScript = myPlant.GetComponent<PlantScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(moisture > -500)
+        //while the moisture is more than the minimum, it's considered "moist"
+        
+        if(moisture > minMoisture)
         {
-            moisture--;
             moist = true;
         } else
         {
             moist = false;
         }
 
-        if(moisture > 1500)
+        //while being drunk from, the soil loses moisture
+        if(plantScript.drinking == true)
         {
-            moisture = 1500;
+            moisture -= 1f * Time.deltaTime;
         }
 
+        //this is the maximum saturation for the soil, any overflow is discarded
+        if(moisture > maxMoisture)
+        {
+            moisture = maxMoisture;
+        }
 
+        //slider decay
         if(drenchingValue > 0)
         {
-            drenchingValue = drenchingValue - 1;
-            if (drenchingValue > 50)
+            //reduce the slider value
+            drenchingValue -= 3f * Time.deltaTime;
+
+            //increase moisture if above a certain value
+            if (drenchingValue > 1)
             {
-                moisture = moisture + 2;
+                moisture += 2f * Time.deltaTime;
             }
+
+            //setting the slider to show the value of the drench after reducing the drench amount
+            drenchSlider.value = drenchingValue;
         }
 
-        drenchSlider.value = drenchingValue;
+        
     }
 
+    //function called when the slider moves
     public void Drench(float scale)
     {
+        //this function will let the script know what the slider says
         drenchingValue = scale;
     }
 }
