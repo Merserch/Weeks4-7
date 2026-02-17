94bada7cb0e567a3bc45d822912b0477e7b708ad
Merserch <merserch@sheridancollege.ca>
2026-02-17 12:34:49 -0500

    Moved a lot of code from Plant script to Plot script Very functional Some UI condition bugs to fix

--- Assets/Scripts/PlantScript.cs
+++ Assets/Scripts/PlantScript.cs
@@ -4,11 +4,7 @@ using System.Collections.Generic;
 
 public class PlantScript : MonoBehaviour
 {
-    //current lifespan and maximum lifespan used for reflecting the plant's growth
-    public float currentLifespan = 0f;
-    public float maxLifespan = 30f;
-    //drinking used by plot script
-    public bool drinking = true;
+    
 
     //get the sprite renderer, the plot it belongs to, the plot script of that plot, and the sprites needed
     public SpriteRenderer sr;
@@ -16,16 +12,17 @@ public class PlantScript : MonoBehaviour
     public PlotScript plotScript;
     public List<Sprite> stage;
 
-    //get the timer component in the world UI
-    public GameObject timerHand;
+    
+
+    //current lifespan and maximum lifespan used for reflecting the plant's growth
+    public float currentLifespan;
+    public float maxLifespan;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //get the sprite renderer so we can change the sprite
         sr = GetComponent<SpriteRenderer>();
-        //get the plot script so we can see if the soil is wet
-        plotScript = myPlot.GetComponent<PlotScript>();
         //set the sprite to the first stage, a little sprout
         sr.sprite = stage[0];
     }
@@ -33,28 +30,7 @@ public class PlantScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //check the moisture of the soil
-        if(plotScript.moisture > 0 && currentLifespan < maxLifespan)
-        {
-            //if it's wet, it may drink and grow
-            currentLifespan += 1f * Time.deltaTime;
-            drinking = true;
-
-            //set the timer hand to reflect the percentage of time left until fully grown
-            Vector3 handRotation = timerHand.transform.eulerAngles;
-            handRotation.z -= 360 / maxLifespan * Time.deltaTime;
-            timerHand.transform.eulerAngles = handRotation;
-        }
-
         
-
-        //if moisture is low, drinking will not change but the lifespan will not increase
-
-        if(plotScript.moist == false)
-        {
-            //if the minimum moisture level is met, the plant will stop drinking
-            drinking = false;
-        }
 
         //set the sprite once reaching a certain age
         if(currentLifespan >= maxLifespan * 1 / 3)
--- Assets/Scripts/PlotScript.cs
+++ Assets/Scripts/PlotScript.cs
@@ -2,6 +2,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlotScript : MonoBehaviour
 {
@@ -14,19 +15,29 @@ public class PlotScript : MonoBehaviour
     //watering system components, drenching value for slider value and slider itself in the scene canvas
     public float drenchingValue;
     public Slider drenchSlider;
+    //button for spawning plant
+    public GameObject spawnPlant;
+    //button for despawning plant
+    public GameObject harvestPlant;
     //the plant object associated with this plot and it's script
+    public GameObject plantPrefab;
     public GameObject myPlant;
     public PlantScript plantScript;
     public GameObject needWater;
+    //the plant spawn point
+    public Vector2 spawnPoint;
+    //drinking used by plot script
+    public bool drinking;
+    //get the timer component in the world UI
+    public GameObject timerHand;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //set to maximum moisture
         moisture = maxMoisture;
-        //get the plant object
-        plantScript = myPlant.GetComponent<PlantScript>();
-
+        spawnPoint = new Vector2(transform.position.x, transform.position.y + 2);
     }
 
     // Update is called once per frame
@@ -42,10 +53,12 @@ public class PlotScript : MonoBehaviour
             moist = false;
         }
 
-        if (myPlant != null)
+        //things to do while the plant is alive
+        if (plantScript != null)
         {
+            
             //while being drunk from, the soil loses moisture
-            if(plantScript.drinking == true)
+            if(drinking == true)
             {
                 moisture -= 1f * Time.deltaTime;
                 needWater.SetActive(false);
@@ -53,6 +66,33 @@ public class PlotScript : MonoBehaviour
             else
             {
                 needWater.SetActive(true);
+            }
+
+            //check the moisture of the soil
+            if(moisture > 0 && plantScript.currentLifespan < plantScript.maxLifespan)
+            {
+                //if it's wet, it may drink and grow
+                plantScript.currentLifespan += 1f * Time.deltaTime;
+                drinking = true;
+
+                //set the timer hand to reflect the percentage of time left until fully grown
+                Vector3 handRotation = timerHand.transform.eulerAngles;
+                handRotation.z -= 360 / plantScript.maxLifespan * Time.deltaTime;
+                timerHand.transform.eulerAngles = handRotation;
+            }
+
+
+            //if moisture is low, drinking will not change but the lifespan will not increase
+
+            if(moist == false)
+            {
+                //if the minimum moisture level is met, the plant will stop drinking
+                drinking = false;
+            }
+
+            if(plantScript.currentLifespan >= plantScript.maxLifespan)
+            {
+                harvestPlant.SetActive(true);
             }
         }
 
@@ -86,5 +126,26 @@ public class PlotScript : MonoBehaviour
     {
         //this function will let the script know what the slider says
         drenchingValue = scale;
+    }
+
+    public void NewPlant()
+    {
+        
+        myPlant = Instantiate(plantPrefab, spawnPoint, Quaternion.identity);
+
+        //get the plant object
+        plantScript = myPlant.GetComponent<PlantScript>();
+        spawnPlant.SetActive(false);
+        //current lifespan and maximum lifespan used for reflecting the plant's growth
+        plantScript.currentLifespan = 0f;
+        plantScript.maxLifespan = 10f;
+        
+    }
+
+    public void HarvestPlant()
+    {
+        Destroy(myPlant);
+        harvestPlant.SetActive(false);
+        spawnPlant.SetActive(true);
     }
 }
